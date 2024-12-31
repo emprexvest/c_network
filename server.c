2cbb4208f62dbd20e2782cb67456b5b950522820
emprexvest <121636284+emprexvest@users.noreply.github.com>
2024-12-31 10:13:46 -0800

    Add files via upload
    Server and client

---
+++ server.c
@@ -0,0 +1,48 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+
+    struct sockaddr_in serverInfo = {0};
+    struct sockaddr_in clientInfo = {0};
+    int clientSize = 0;
+
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_addr.s_addr = 0;
+    serverInfo.sin_port = htons(5555);
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    // Bind
+    if (bind(fd, (struct sockaddr*)&serverInfo, sizeof(serverInfo)) == -1 ) {
+        perror("bind");
+        close(fd);
+        return -1;
+    }
+
+    // Listen
+    if (listen(fd, 0) == -1 ) {
+        perror("listen");
+        close(fd);
+        return -1;
+    }
+
+    // Accept
+    int cfd = accept(fd, (struct sockaddr*)&clientInfo, &clientSize);
+    if (cfd == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    close(cfd);
+
+}
